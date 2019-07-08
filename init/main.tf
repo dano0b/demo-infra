6ff12057c3d69e047e59daf0ff77f8f7ed4a0e0b
Adam Lemanski <adam.lemanski@gmail.com>
2019-07-08 18:55:33 +0700

    use s3 remote state since aws needs to be used

--- init/main.tf
+++ init/main.tf
@@ -1,10 +1,13 @@
-provider "google" {
-  credentials = "../account.json"
-  project     = "adam-demo-ekoapp"
-  region      = "us-east1"
+provider "aws" {
+  version = "~> 2.0"
+  region  = "ap-southeast-1"
 }
 
-resource "google_storage_bucket" "terrafrom-state" {
-  name     = "terrafrom-state"
-  location = "us-east1"
+resource "aws_s3_bucket" "terraform-state" {
+  bucket = "terraform-state-demo-ekoapp"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
 }
--- main.tf
+++ main.tf
@@ -1,13 +1,21 @@
 terraform {
-  backend "gcs" {
-    credentials = "account.json"
-    bucket      = "terrafrom-state"
-    prefix      = "base"
+  backend "s3" {
+    bucket = "terraform-state-demo-ekoapp"
+    key    = "terraform-state"
+    region = "ap-southeast-1"
   }
 }
 
-provider "google" {
-  credentials = "account.json"
-  project     = "adam-demo-ekoapp"
-  region      = "us-east1"
+provider "aws" {
+  version = "~> 2.0"
+  region  = "ap-southeast-1"
+}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-state-demo-ekoapp"
+    key    = "terraform-state"
+    region = "ap-southeast-1"
+  }
 }
